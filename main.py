656b51f69314a27df822eb82d73aead9e48d41b3
keyboard2005 <samuraizd@outlook.com>
2025-04-29 03:28:45 +0800

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 from PIL import ImageOps
 from screeninfo import get_monitors
 import os
+import random
 
 
 def set_wallpaper(path, mode="spanned"):
@@ -14,22 +15,10 @@ dconf write /org/gnome/desktop/background/picture-options "'{mode}'"
 
 
 class MultipleMonitorsWallpaperManager:
-    def __init__(self, wallpapers):
+    def __init__(self):
         self.width = 0
         self.height = 0
         self.positions = []
-        self.wallpapers = [os.path.abspath(wallpaper) for wallpaper in wallpapers]
-        self.run()
-
-    def resize_and_pad(self, img, size):
-        """将 img 等比缩放到 size，空白填充为 color"""
-        return ImageOps.pad(img, size, color=(0,0,0), method=Image.LANCZOS)
-
-    def resize_and_crop(self, img, size):
-        """将 img 等比缩放并裁剪到 size"""
-        return ImageOps.fit(img, size, method=Image.LANCZOS)
-    
-    def run(self):
         for m in get_monitors():
             self.width += m.width
             self.height = max(self.height, m.height)
@@ -40,12 +29,21 @@ class MultipleMonitorsWallpaperManager:
                 'height': m.height
             })
 
+    def resize_and_pad(self, img, size):
+        """将 img 等比缩放到 size，空白填充为 color"""
+        return ImageOps.pad(img, size, color=(0,0,0), method=Image.LANCZOS)
+
+    def resize_and_crop(self, img, size):
+        """将 img 等比缩放并裁剪到 size"""
+        return ImageOps.fit(img, size, method=Image.LANCZOS)
+    
+    def set_wallpaper(self, wallpapers):
         # 1. 创建指定大小的黑色图片
         black_img = Image.new('RGB', (self.width, self.height), color=(0, 0, 0))
 
         # 3. 设置图片到x,y位置
         for i, item in enumerate(self.positions):
-            img_path = self.wallpapers[i]
+            img_path = wallpapers[i]
             if not os.path.exists(img_path):
                 print(f"图片 {img_path} 不存在，跳过。")
                 continue  # 跳过
@@ -59,5 +57,26 @@ class MultipleMonitorsWallpaperManager:
         black_img.save('result.png')
         set_wallpaper(os.path.join(os.getcwd(), 'result.png'), mode='spanned')
 
+    def set_wallpaper_random(self, wallpaper_folder):
+        # 1. 获取文件夹下所有图片
+        images = [os.path.join(wallpaper_folder, img) for img in os.listdir(wallpaper_folder) if img.endswith(('.png', '.jpg', '.jpeg'))]
+        # 2. 随机选择len(self.positions)张图片
+        if len(images) < len(self.positions):
+            print(f"图片数量不足，至少需要 {len(self.positions)} 张图片。")
+            return
+        random_images = random.sample(images, len(self.positions))
+        # 3. 设置壁纸
+        self.set_wallpaper(random_images)
 
-MultipleMonitorsWallpaperManager(wallpapers=['./images/0.jpg', './images/1.jpg', './images/2.jpg'])
+
+m = MultipleMonitorsWallpaperManager()
+
+# m.set_wallpaper(
+#     wallpapers = [
+#         './images/3.png',
+#         './images/4.png',
+#         './images/5.png'
+#     ]
+# )
+
+m.set_wallpaper_random(wallpaper_folder='./images')
